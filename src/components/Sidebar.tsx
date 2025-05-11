0bd7b44d0fc3d6f04ddde5d0ee90c143475bb3c9
acasasis-dev <angelito.casasis@gmail.com>
2025-05-11 21:51:55 +0800

    Added styled sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+	return (
+		<aside>
+			<h2 className="sidebar-title">Your Projects</h2>
+			<div className="add-project-btn">+ Add Project</div>
+		</aside>
+	)
+}
